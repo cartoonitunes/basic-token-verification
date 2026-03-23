2fbc1660d19a67fda97df3ef8bd862748d3e0980
cartoonitunes <181663414+cartoonitunes@users.noreply.github.com>
2026-03-23 01:54:50 -0400

    Add Coin (83 siblings) and Token 0.1 fixed (103 siblings)

---
+++ src/Coin.sol
@@ -0,0 +1,16 @@
+contract Coin {
+    mapping (address => uint) public coinBalanceOf;
+    event CoinTransfer(address sender, address receiver, uint amount);
+    
+    function Coin(uint supply) {
+        coinBalanceOf[msg.sender] = supply;
+    }
+    
+    function sendCoin(address receiver, uint amount) returns(bool sufficient) {
+        if (coinBalanceOf[msg.sender] < amount) return false;
+        coinBalanceOf[msg.sender] -= amount;
+        coinBalanceOf[receiver] += amount;
+        CoinTransfer(msg.sender, receiver, amount);
+        return true;
+    }
+}
